1804e34179c4ef312b6c6d59f38f96f667f2910b
yupu <pyl198403@163..com>
2020-12-01 16:49:06 +0800

    删除无用文件

--- thales-scheduler/scripts/start-worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-script_folder=$(cd `dirname $0`;pwd)
-parent_dir="$(dirname "$script_folder")"
-base_dir="$(dirname "$parent_dir")"
-java -jar $base_dir/target/scheduler-worker-0.0.1-SNAPSHOT.jar
-code=$?
-exit $code
